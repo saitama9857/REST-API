60fcefe8a7bdb1e2100be6303e480d41bfac9cc7
saitama9857 <ermamatov2001@gmail.com>
2021-02-26 11:13:38 +0600

    first

--- app.js
+++ app.js
@@ -1,16 +1,81 @@
+
+const Joi = require('joi');
 const express = require('express');
-const path = require('path');
 const app = express();
+
+
+app.use(express.json());
+
+const courses = [
+     {id: 1, name: 'course1'},
+     {id: 2, name: 'course2'},
+     {id: 3, name: 'course3'}
+]
+    
+
+
+
+//ROUTERS
+app.get('/', (req , res) =>{
+      res.send('WE ARE IN LOCALLHOST');
+});
+app.get('/api/courses', (req,   res)=>{
+     res.send(courses);
+});
+
+app.post('/api/courses', (req, res)=>{
+     const {error} = validateCourse(req.body);
+     if(error)return res.status(400).send(error.details[0].message)
+        
+    
+     const course = {
+          id: courses.length + 1,
+          name : req.body.name
+     }
+     courses.push(course);
+     res.send(course);
+});
+
+app.put('api/courses/:id', (req , res)=>{  
+     const course = courses.find(c => c.id === parseInt(req.params.id));
+     if(!course)return res.status(404).send('The courses with given ID was not found')
+     
+
+     const {error} = validateCourse(req.body);
+     if(error)return res.status(400).send(error.details[0].message)
+        
+   
+     course.name = req.body.name;
+     res.send(course);
+}); 
+function validateCourse (course){
+     const schema = {
+          name: Joi.string().min(3).required(),
+     }
+        return Joi.validate(course, schema);
+};
+
+app.delete('/api/courses/:id', (req,res)=>{
+     const course = courses.find(c => c.id === parseInt(req.params.id));
+     if(!course) return res.status(404).send('The courses with given ID was not found')
+
+    const index = courses.indexOf(courses);
+    courses.slice(index, 1);
+    res.send(course);
+
+})
+ 
+
+
+
+app.get('/api/courses/:id', (req, res)=>{
+     const course = courses.find(c => c.id === parseInt(req.params.id));
+     if(!course)return res.status(404).send('The courses with given ID was not found')
+          res.send(course);
+});
+//we listen the server
 const PORT = process.env.PORT || 80;
-
-app.use(express.static(path.resolve(__dirname, 'clieant')))
-
-app.get('*',(req, res)=>{
-     res.sendFile(path.resolve(__dirname, 'clieant', 'index.html'));
-
-})
-app.listen(PORT, ()=>{
-     console.log('server is working 80 ...');
-})
-
+app.listen(PORT , () =>{
+     console.log('here we go again');
+});
 
